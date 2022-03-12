af98fdcf60f475e60573efd53d9a3fbe0192f76e
irrelevantRyan <ryanz22@nycstudents.net>
2022-03-12 18:47:29 -0500

    Google sheets code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import gspread
 import streamlit as st
 import streamlit_authenticator as stauth
 from streamlit_option_menu import option_menu
@@ -52,16 +53,36 @@ if authentication_status:
                 date = datetime.now().strftime("%d/%m/%Y")
                 time = datetime.now().strftime("%H:%M:%S")
 
-                # Write in database
-                    # Code
+                #Google API credentials from st.secrets
+                google_api_credentials = {
+                    "type": st.secrets['type'],
+                    "project_id": st.secrets['project_id'],
+                    "private_key_id": st.secrets['private_key_id'],
+                    "private_key": st.secrets['private_key'],
+                    "client_email":st.secrets['client_email'],
+                    "client_id": st.secrets['client_id'],
+                    "auth_uri": st.secrets['auth_uri'],
+                    "token_uri": st.secrets['token_uri'],
+                    "auth_provider_x509_cert_url": st.secrets['auth_provider_x509_cert_url'],
+                    "client_x509_cert_url": st.secrets['client_x509_cert_url'],
+                }
+
+                # # Save API calls because I am broke
+                # sa = gspread.service_account_from_dict(google_api_credentials)
+                # sh = sa.open('shoreline-reconfiguration')
+                # wks = sh.worksheet('data')
+
+                #Write the data into database
+                # cell = wks.find(name)
+                # body = [date, time, age, alge, shape, review]
+                # wks.append_row(body, table_range="A1:G1")
+
 
                 st.success(f"Submission Time: {date} {time}. Hope you have a great day! 😊")
                 st.balloons()
             else:
                 st.warning(f"Sorry {name} you are incorrect. Please try again.")
 
-        
-        
         
     elif selected == "Leader Board":
         # Leader Board
